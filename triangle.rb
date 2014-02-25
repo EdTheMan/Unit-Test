ccf4ce2024a0efe41f56ea4f7df2c0acbdeaa23a
EdTheMan <chiieong@brandeis.edu>
2014-02-25 00:46:45 -0500

    Finished

--- triangle.rb
+++ triangle.rb
@@ -19,7 +19,7 @@ class Triangle
   
   #returns the area of a triangle
   def area
-    return (( (p1.x*(p2.y - p3.y)) + (p2.x*(p3.y - p1.y)) + (p3.x*(p1.y - p2.y)) )/2.0).abs
+    return (( (@p1.x*(@p2.y - @p3.y)) + (@p2.x*(@p3.y - @p1.y)) + (@p3.x*(@p1.y - @p2.y)) )/2.0).abs
   end
 
 
